30d382faa00f2b90861f06eff11c8827b68ed6ee
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-03 00:40:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,15 +13,15 @@ class Triangle
    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
         raise
         TriangleError
-      end
-    if @side1 == @side2 && @side3 == @side1
-      :equilateral
+    else
+      if @side1 == @side2 && @side3 == @side1
+        :equilateral
       
-    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-      :isosceles
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
     
-    else 
-      :scalene
+      else 
+        :scalene
     end
    
   end
