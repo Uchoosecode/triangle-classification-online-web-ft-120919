4caaa64b5806d69dee669d8c4869e0e649093306
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-03 00:34:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
     
-    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+    elsif 
       :scalene
     else 
       if @side1 == 0 || @side2 == 0 || @side3 == 0 
