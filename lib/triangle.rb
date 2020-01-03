817171d72010042a51d51decdcc2163df8f61f31
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-03 00:48:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,12 +10,13 @@ class Triangle
  end
   
   def kind 
-   if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
-        raise
-        TriangleError
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
+      raise
+      TriangleError
     
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise TriangleError
+      raise 
+      TriangleError
     
     else
       if @side1 == @side2 && @side3 == @side1
